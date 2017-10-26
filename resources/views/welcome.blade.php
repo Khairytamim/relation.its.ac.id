eb60cda5087411d733dbb0f5b9a615d95267b5e6
Fikry Khairytamim <fikry.labsky08@gmail.com>
2017-10-26 21:07:40 +0700

    bug resolved

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -428,20 +428,20 @@ $( "#sent" ).submit(function( event ) {
         // console.log($posting);    
     });
     // Scrolls to the selected menu item on the page
-    $(function() {
-        $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html,body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
-            }
-        });
-    });
+    // $(function() {
+    //     $('a[href*=#]:not([href=#],[data-toggle],[data-target],[data-slide])').click(function() {
+    //         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             if (target.length) {
+    //                 $('html,body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000);
+    //                 return false;
+    //             }
+    //         }
+    //     });
+    // });
 </script>
 <script type="text/javascript">
     $('selector li.dropdown').hover(function() {
